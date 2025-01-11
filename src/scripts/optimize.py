d07f847cf2d58798aa15a01decccb076cd196280
Valérian Rey <valerian.rey@gmail.com>
2025-01-11 18:17:34 +0100

    Move constants to constants.py

--- src/scripts/optimize.py
+++ src/scripts/optimize.py
@@ -20,82 +20,18 @@ import warnings
 import numpy as np
 import torch
 from docopt import docopt
-from torchjd.aggregation import (
-    IMTLG,
-    MGDA,
-    AlignedMTL,
-    CAGrad,
-    DualProj,
-    GradDrop,
-    Mean,
-    NashMTL,
-    PCGrad,
-    Random,
-    UPGrad,
-)
 
-from trajectories.objectives import ElementWiseQuadratic, QuadraticForm
+from trajectories.constants import (
+    AGGREGATORS,
+    BASE_LEARNING_RATES,
+    INITIAL_POINTS,
+    LR_MULTIPLIERS,
+    OBJECTIVES,
+)
 from trajectories.optimization import optimize
 from trajectories.paths import RESULTS_DIR, get_params_dir, get_values_dir
 
 warnings.filterwarnings("ignore")
-
-AGGREGATORS = {
-    "upgrad": UPGrad(),
-    "mgda": MGDA(),
-    "cagrad": CAGrad(c=0.5),
-    "nashmtl": NashMTL(n_tasks=2),
-    "graddrop": GradDrop(),
-    "imtl_g": IMTLG(),
-    "aligned_mtl": AlignedMTL(),
-    "dualproj": DualProj(),
-    "pcgrad": PCGrad(),
-    "random": Random(),
-    "mean": Mean(),
-}
-BASE_LEARNING_RATES = {
-    "EWQ-2": 0.075,
-    "QF_v1": 0.01,
-}
-LR_MULTIPLIERS = {
-    "upgrad": 1.0,
-    "mgda": 1.0,
-    "cagrad": 1.0,
-    "nashmtl": 2.0,
-    "graddrop": 0.5,
-    "imtl_g": 2.0,
-    "aligned_mtl": 1.0,
-    "dualproj": 1.0,
-    "pcgrad": 0.5,
-    "random": 1.0,
-    "mean": 1.0,
-}
-
-OBJECTIVES = {
-    "EWQ-2": ElementWiseQuadratic(2),
-    "QF_v1": QuadraticForm(
-        As=[4 * torch.tensor([[1.0, -1.0], [-1.0, 4.0]]), torch.tensor([[1.0, -2.0], [1.0, -1.0]])],
-        us=[torch.tensor([1.0, -1.0]), torch.tensor([0.0, 0.0])],
-    ),
-}
-INITIAL_POINTS = {
-    "EWQ-2": [
-        [3.0, -2],
-        [0.0, -3.0],
-        [-4.0, 4.0],
-        [-3.0, 4.0],
-        [1.0, 5.0],
-        [-5.0, -1.0],
-    ],
-    "QF_v1": [
-        [3.0, -2],
-        [0.0, -3.0],
-        [-4.0, 4.0],
-        [-3.0, 4.0],
-        [1.0, 5.0],
-        [-5.0, -1.0],
-    ],
-}
 
 
 def main():
---
+++ src/trajectories/constants.py
@@ -0,0 +1,73 @@
+import torch
+from torchjd.aggregation import (
+    IMTLG,
+    MGDA,
+    AlignedMTL,
+    CAGrad,
+    DualProj,
+    GradDrop,
+    Mean,
+    NashMTL,
+    PCGrad,
+    Random,
+    UPGrad,
+)
+
+from trajectories.objectives import ElementWiseQuadratic, QuadraticForm
+
+AGGREGATORS = {
+    "upgrad": UPGrad(),
+    "mgda": MGDA(),
+    "cagrad": CAGrad(c=0.5),
+    "nashmtl": NashMTL(n_tasks=2),
+    "graddrop": GradDrop(),
+    "imtl_g": IMTLG(),
+    "aligned_mtl": AlignedMTL(),
+    "dualproj": DualProj(),
+    "pcgrad": PCGrad(),
+    "random": Random(),
+    "mean": Mean(),
+}
+LR_MULTIPLIERS = {
+    "upgrad": 1.0,
+    "mgda": 1.0,
+    "cagrad": 1.0,
+    "nashmtl": 2.0,
+    "graddrop": 0.5,
+    "imtl_g": 2.0,
+    "aligned_mtl": 1.0,
+    "dualproj": 1.0,
+    "pcgrad": 0.5,
+    "random": 1.0,
+    "mean": 1.0,
+}
+
+OBJECTIVES = {
+    "EWQ-2": ElementWiseQuadratic(2),
+    "QF_v1": QuadraticForm(
+        As=[4 * torch.tensor([[1.0, -1.0], [-1.0, 4.0]]), torch.tensor([[1.0, -2.0], [1.0, -1.0]])],
+        us=[torch.tensor([1.0, -1.0]), torch.tensor([0.0, 0.0])],
+    ),
+}
+BASE_LEARNING_RATES = {
+    "EWQ-2": 0.075,
+    "QF_v1": 0.01,
+}
+INITIAL_POINTS = {
+    "EWQ-2": [
+        [3.0, -2],
+        [0.0, -3.0],
+        [-4.0, 4.0],
+        [-3.0, 4.0],
+        [1.0, 5.0],
+        [-5.0, -1.0],
+    ],
+    "QF_v1": [
+        [3.0, -2],
+        [0.0, -3.0],
+        [-4.0, 4.0],
+        [-3.0, 4.0],
+        [1.0, 5.0],
+        [-5.0, -1.0],
+    ],
+}
