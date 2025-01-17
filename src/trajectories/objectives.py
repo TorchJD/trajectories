6e89a23ca1dfd71e14d4f3eb769da8f36e96275e
Valérian Rey <valerian.rey@gmail.com>
2025-01-18 00:09:06 +0100

    Make QCF extend QF

--- src/trajectories/objectives.py
+++ src/trajectories/objectives.py
@@ -28,6 +28,44 @@ class Objective(ABC):
         return f"{self.__class__.__name__}({self.n_values})"
 
 
+class QuadraticForm(Objective):
+    def __init__(self, As: list[Tensor], us: list[Tensor]):
+        if len(As) != len(us):
+            raise ValueError("As and us must have the same length.")
+
+        if len(As) < 1:
+            raise ValueError("As and us must have at least one element.")
+
+        super().__init__(n_params=len(us[0]), n_values=len(As))
+        # Note that if A is not PSD, the objective is not convex.
+        self.As = As
+        self.us = us
+
+    def __call__(self, x: Tensor) -> Tensor:
+        objective_values = [self.quad(x, A, u) for A, u in zip(self.As, self.us)]
+        return torch.stack(objective_values)
+
+    def jacobian(self, x: Tensor) -> Tensor:
+        return torch.vstack([2 * (x - u) @ A for A, u in zip(self.As, self.us)])
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(As={self.As}, us={self.us})"
+
+    @staticmethod
+    def quad(x: Tensor, A: Tensor, u: Tensor):
+        x_minus_u = x - u
+        return x_minus_u @ A @ x_minus_u
+
+
+class ConvexQuadraticForm(QuadraticForm):
+    def __init__(self, Bs: list[Tensor], us: list[Tensor]):
+        self.Bs = Bs
+        super().__init__(As=[B @ B.T for B in self.Bs], us=us)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(Bs={self.Bs}, us={self.us})"
+
+
 class ElementWiseQuadratic(Objective):
     def __init__(self, n_dim: int):
         super().__init__(n_params=n_dim, n_values=n_dim)
@@ -39,59 +77,3 @@ class ElementWiseQuadratic(Objective):
 
     def jacobian(self, x: Tensor) -> Tensor:
         return torch.diagonal(torch.stack([2 * x[0], 2 * x[1]]))
-
-
-class QuadraticForm(Objective):
-    def __init__(self, As: list[Tensor], us: list[Tensor]):
-        if len(As) != len(us):
-            raise ValueError("As and us must have the same length.")
-
-        if len(As) < 1:
-            raise ValueError("As and us must have at least one element.")
-
-        super().__init__(n_params=len(us[0]), n_values=len(As))
-        # Note that if A is not PSD, the objective is not guaranteed to be convex (or maybe it's
-        # even guaranteed to not be convex). To force the As to be PSD, use ConvexQuadraticForm.
-        self.As = As
-        self.us = us
-
-    def __call__(self, x: Tensor) -> Tensor:
-        objective_values = [quad(x, A, u) for A, u in zip(self.As, self.us)]
-        return torch.stack(objective_values)
-
-    def jacobian(self, x: Tensor) -> Tensor:
-        return torch.vstack([2 * (x - u) @ A for A, u in zip(self.As, self.us)])
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(As={self.As}, us={self.us})"
-
-
-class ConvexQuadraticForm(Objective):
-    def __init__(self, Bs: list[Tensor], us: list[Tensor]):
-        if len(Bs) != len(us):
-            raise ValueError("Bs and us must have the same length.")
-
-        if len(Bs) < 1:
-            raise ValueError("Bs and us must have at least one element.")
-
-        super().__init__(n_params=len(us[0]), n_values=len(Bs))
-        self.Bs = Bs
-        self.us = us
-
-        # Precompute As to save computation time.
-        self.As = [B @ B.T for B in self.Bs]
-
-    def __call__(self, x: Tensor) -> Tensor:
-        objective_values = [quad(x, A, u) for A, u in zip(self.As, self.us)]
-        return torch.stack(objective_values)
-
-    def jacobian(self, x: Tensor) -> Tensor:
-        return torch.vstack([2 * (x - u) @ A for A, u in zip(self.As, self.us)])
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(Bs={self.Bs}, us={self.us})"
-
-
-def quad(x: Tensor, A: Tensor, u: Tensor):
-    x_minus_u = x - u
-    return x_minus_u @ A @ x_minus_u
