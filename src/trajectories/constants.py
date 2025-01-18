f13487f571897dd9c4e48713fb530615d5adcf55
Valérian Rey <valerian.rey@gmail.com>
2025-01-18 18:03:34 +0100

    Add CQF_v3

--- src/trajectories/constants.py
+++ src/trajectories/constants.py
@@ -1,3 +1,5 @@
+from math import cos, sin
+
 import torch
 from torchjd.aggregation import (
     IMTLG,
@@ -42,6 +44,8 @@ LR_MULTIPLIERS = {
     "mean": 1.0,
 }
 
+THETA = 0.1
+
 OBJECTIVES = {
     "EWQ-2": ElementWiseQuadratic(2),
     "QF_v1": QuadraticForm(
@@ -62,12 +66,22 @@ OBJECTIVES = {
         ],
         us=[torch.tensor([1.0, 0.0]), torch.tensor([0.0, 1.0])],
     ),
+    "CQF_v3": ConvexQuadraticForm(
+        Bs=[
+            torch.tensor([[cos(THETA), -sin(THETA)], [sin(THETA), cos(THETA)]])
+            @ torch.diag(torch.tensor([1.0, 0.0])),
+            torch.tensor([[cos(THETA), sin(THETA)], [-sin(THETA), cos(THETA)]])
+            @ torch.diag(torch.tensor([2.0, 0.0])),
+        ],
+        us=[torch.tensor([1.0, 0.0]), torch.tensor([0.0, 0.0])],
+    ),
 }
 BASE_LEARNING_RATES = {
     "EWQ-2": 0.075,
     "QF_v1": 0.01,
     "CQF_v1": 0.1,
     "CQF_v2": 0.002,
+    "CQF_v3": 0.02,
 }
 INITIAL_POINTS = {
     "EWQ-2": [
@@ -106,10 +120,15 @@ INITIAL_POINTS = {
         [1.5, 0.0],
         [1.0, -0.5],
     ],
+    "CQF_v3": [
+        [0.8, 0.0],
+        [0.5, 0.0],
+    ],
 }
 N_ITERS = {
     "EWQ-2": 50,
     "QF_v1": 100,
     "CQF_v1": 200,
     "CQF_v2": 500,
+    "CQF_v3": 3500,
 }
