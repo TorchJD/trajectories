d9d553d8ac08544f845fa08cc70b02daad39a5ae
Valérian Rey <valerian.rey@gmail.com>
2025-01-26 00:09:04 +0100

    Remove outdated objectives
    - QF_v1
    - CQF_v1
    - CQF_v2
    - CQF_v3
    - CQF_v4

--- src/trajectories/constants.py
+++ src/trajectories/constants.py
@@ -16,7 +16,7 @@ from torchjd.aggregation import (
     UPGrad,
 )
 
-from trajectories.objectives import ConvexQuadraticForm, ElementWiseQuadratic, QuadraticForm
+from trajectories.objectives import ConvexQuadraticForm, ElementWiseQuadratic
 
 AGGREGATORS = {
     "upgrad": UPGrad(reg_eps=1e-7, norm_eps=1e-9),
@@ -71,47 +71,10 @@ LATEX_NAMES = {
     "mean": r"$\mathcal A_{\mathrm{Mean}}$",
 }
 
-THETA = 0.1
 THETA_v5 = np.pi / 16
 
 OBJECTIVES = {
     "EWQ-2": ElementWiseQuadratic(2),
-    "QF_v1": QuadraticForm(
-        As=[4 * torch.tensor([[1.0, -1.0], [-1.0, 4.0]]), torch.tensor([[1.0, -2.0], [1.0, -1.0]])],
-        us=[torch.tensor([1.0, -1.0]), torch.tensor([0.0, 0.0])],
-    ),
-    "CQF_v1": ConvexQuadraticForm(
-        Bs=[
-            0.1 * torch.tensor([[1.0, -1.0], [-1.0, 4.0]]),
-            torch.tensor([[1.0, -2.0], [1.0, -1.0]]),
-        ],
-        us=[torch.tensor([1.0, -1.0]), torch.tensor([-1.0, -3.0])],
-    ),
-    "CQF_v2": ConvexQuadraticForm(
-        Bs=[
-            4 * torch.tensor([[2.0, 1.0], [1.0, 2.0]]),
-            torch.tensor([[2.0, 1.0], [1.0, -2.0]]),
-        ],
-        us=[torch.tensor([1.0, 0.0]), torch.tensor([0.0, 1.0])],
-    ),
-    "CQF_v3": ConvexQuadraticForm(
-        Bs=[
-            torch.tensor([[cos(THETA), -sin(THETA)], [sin(THETA), cos(THETA)]])
-            @ torch.diag(torch.tensor([1.0, 0.0])),
-            torch.tensor([[cos(THETA), sin(THETA)], [-sin(THETA), cos(THETA)]])
-            @ torch.diag(torch.tensor([2.0, 0.0])),
-        ],
-        us=[torch.tensor([1.0, 0.0]), torch.tensor([0.0, 0.0])],
-    ),
-    "CQF_v4": ConvexQuadraticForm(
-        Bs=[
-            torch.tensor([[cos(THETA), -sin(THETA)], [sin(THETA), cos(THETA)]])
-            @ torch.diag(torch.tensor([1.0, 0.1])),
-            torch.tensor([[cos(THETA), sin(THETA)], [-sin(THETA), cos(THETA)]])
-            @ torch.diag(torch.tensor([1.5, 0.1])),
-        ],
-        us=[torch.tensor([1.0, 0.0]), torch.tensor([0.0, 0.0])],
-    ),
     "CQF_v5": ConvexQuadraticForm(
         Bs=[
             torch.tensor([[cos(THETA_v5), -sin(THETA_v5)], [sin(THETA_v5), cos(THETA_v5)]])
@@ -124,11 +87,6 @@ OBJECTIVES = {
 }
 BASE_LEARNING_RATES = {
     "EWQ-2": 0.075,
-    "QF_v1": 0.01,
-    "CQF_v1": 0.1,
-    "CQF_v2": 0.002,
-    "CQF_v3": 0.1,
-    "CQF_v4": 0.1,
     "CQF_v5": 0.02,
 }
 INITIAL_POINTS = {
@@ -139,54 +97,6 @@ INITIAL_POINTS = {
         [-3.0, 4.0],
         [-3.5, -0.75],
     ],
-    "QF_v1": [
-        [3.0, -2.0],
-        [0.0, -3.0],
-        [-4.0, 4.0],
-        [-3.0, 4.0],
-        [1.0, 5.0],
-        [-5.0, -1.0],
-    ],
-    "CQF_v1": [
-        [3.0, -2.0],
-        [0.0, -3.0],
-        [-4.0, 4.0],
-        [-3.0, 4.0],
-        [1.0, 5.0],
-        [-5.0, -1.0],
-    ],
-    "CQF_v2": [
-        [0.0, 0.0],
-        [-0.5, 0.0],
-        [-0.5, 0.5],
-        [-0.5, 1.0],
-        [0.0, 1.5],
-        [0.5, 1.25],
-        [1.0, 1.0],
-        [1.5, 0.5],
-        [1.5, 0.0],
-        [1.0, -0.5],
-    ],
-    "CQF_v3": [
-        [0.8, 0.0],
-        [0.5, 0.0],
-        [0.25, 7.0],
-        [0.5, 7.0],
-        [0.75, 7.0],
-        [0.0, 4.0],
-        [1.0, 5.0],
-        [0.0625, 0.0],
-    ],
-    "CQF_v4": [
-        [0.8, 0.0],
-        [0.5, 0.0],
-        [0.25, 7.0],
-        [0.5, 7.0],
-        [0.75, 7.0],
-        [0.0, 4.0],
-        [1.0, 5.0],
-        [0.0625, 0.0],
-    ],
     "CQF_v5": [
         [0.5, 0.5],
         [-1.0, 7.0],
@@ -196,19 +106,9 @@ INITIAL_POINTS = {
 }
 N_ITERS = {
     "EWQ-2": 50,
-    "QF_v1": 100,
-    "CQF_v1": 200,
-    "CQF_v2": 500,
-    "CQF_v3": 1000,
-    "CQF_v4": 1000,
     "CQF_v5": 2500,
 }
 N_SAMPLES_SPSM = {
     "EWQ-2": 1,
-    "QF_v1": 100,
-    "CQF_v1": 100,
-    "CQF_v2": 100,
-    "CQF_v3": 100,
-    "CQF_v4": 100,
     "CQF_v5": 100,
 }
